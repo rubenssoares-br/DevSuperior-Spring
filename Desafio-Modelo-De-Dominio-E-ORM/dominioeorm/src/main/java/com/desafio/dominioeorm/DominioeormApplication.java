80f23ead7f4135d500bd7c38733082c2dd92bc05
Rubens Soares <rubens.soares@liferay.com>
2026-08-10 14:13:50 -0300

    Add Desafio-Modelo-De-Dominio-E-ORM - spring initializr

---
+++ Desafio-Modelo-De-Dominio-E-ORM/dominioeorm/src/main/java/com/desafio/dominioeorm/DominioeormApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.dominioeorm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DominioeormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DominioeormApplication.class, args);
+	}
+
+}
---
+++ Desafio-Modelo-De-Dominio-E-ORM/dominioeorm/src/test/java/com/desafio/dominioeorm/DominioeormApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.dominioeorm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DominioeormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
